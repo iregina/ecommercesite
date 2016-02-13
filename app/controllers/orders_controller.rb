f65e74e17e4be4ca68a9ce3c3dd1db5f3aa71ff0
Ovi Calvo & Regina Wong & Michael <pair+0viWan.iregina.whelpley@devbootcamp.com>
2016-02-13 14:07:56 -0800

    fix checkout bugs

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,10 @@
 class OrdersController < ApplicationController
   include ApplicationHelper
 
+  def index
+    # @orders = Order.where _____
+  end
+
   def create
     @order = Order.new(user_id: current_user.id)
   end
@@ -9,9 +13,30 @@ class OrdersController < ApplicationController
     @order = current_shopping_cart
   end
 
-  def edit
-    @order = Order.find(params[:id])
+  def checkout
+    @order = current_shopping_cart
+    pass_the_id = @order.id
+    @order.check_out
+    # p "*********************************"
+    # p @order
+    # p @order.purchased_items.all
+    # p @order.complete
+    # p "*********************************"
+    @order = Order.find(pass_the_id)
+    if @order.complete
+      flash[:success] = "You checked out successfully!"
+    else
+      flash[:error] = "Something went terribly wrong!"
+      # "There is not enough in stock of one of the items you ordered (go figure out which one yourself), please edit the quantity in the shopping cart."
+    end
+    redirect_to '/'
+    # redirect_to 'thank_you'
+    # redirect_to checkout_path
   end
+
+  # def thank_you
+
+  # end
 
 end
 
